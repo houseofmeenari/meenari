1b518c474457ad31dedc191a9945f927fb224e0d
meenari <houseofmeenari@gmail.com>
2019-02-02 22:38:18 -0500

    fin: cart to bag, about, contact, policy, footer, login, links | still: currency, emails, etc.

--- wp-content/themes/karo-child/functions.php
+++ wp-content/themes/karo-child/functions.php
@@ -39,3 +39,53 @@ if( ! function_exists( 'ftc_cookies_popup' ) ) {
 }
 
 // END ENQUEUE PARENT ACTION
+
+add_filter( 'add_to_cart_text', 'woo_custom_cart_button_text');
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text');
+add_filter( 'woocommerce_product_add_to_cart_text', 'woo_custom_cart_button_text');
+
+function woo_custom_cart_button_text() {
+    return __('Add to Bag', 'woocommerce');
+}
+
+function woo_custom_change_cart_string($translated_text, $text, $domain) {
+	$translated_text = str_replace("cart", "bag", $translated_text);
+	$translated_text = str_replace("Cart", "Bag", $translated_text);
+	$translated_text = str_replace("View Cart", "View Bag", $translated_text);
+return $translated_text;
+}
+add_filter('gettext', 'woo_custom_change_cart_string', 100, 3);
+add_filter('ngettext', 'woo_custom_change_cart_string', 100, 3);
+
+// Cart - Basket Text
+function wpa_change_my_basket_text( $translated_text, $text, $domain ){
+    if( $domain == 'woothemes' && $translated_text == 'Cart:' )
+        $translated_text = 'Basket:';
+    return $translated_text;
+}
+add_filter( 'gettext', 'wpa_change_my_basket_text', 10, 3 );
+
+// test this functionality: Is it adding the bag? -- or is the one above adding the bag still? 
+add_filter( 'woocommerce_product_add_to_cart_text', 'woo_archive_custom_cart_button_icon' );    
+function woo_archive_custom_cart_button_icon() {
+        return '<i class="fa fa-shopping-bag" aria-hidden="true"></i>';
+}
+
+// restration form fields:
+    function wooc_extra_register_fields() {?>
+        <p class="form-row form-row-wide">
+        <label for="reg_billing_phone"><?php _e( 'Phone', 'woocommerce' ); ?></label>
+        <input type="text" class="input-text" name="billing_phone" id="reg_billing_phone" value="<?php esc_attr_e( $_POST['billing_phone'] ); ?>" />
+        </p>
+        <p class="form-row form-row-first">
+        <label for="reg_billing_first_name"><?php _e( 'First name', 'woocommerce' ); ?><span class="required">*</span></label>
+        <input type="text" class="input-text" name="billing_first_name" id="reg_billing_first_name" value="<?php if ( ! empty( $_POST['billing_first_name'] ) ) esc_attr_e( $_POST['billing_first_name'] ); ?>" />
+        </p>
+        <p class="form-row form-row-last">
+        <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?><span class="required">*</span></label>
+        <input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?>" />
+        </p>
+        <div class="clear"></div>
+        <?php
+  }
+  add_action( 'woocommerce_register_form_start', 'wooc_extra_register_fields' );
